cdedfc46324594d3535304630e771548ac4c7597
Maulana Akbar <67249292+maulanaakbardj@users.noreply.github.com>
2023-04-09 12:36:36 +0700

    Create APIChatGPT_m2.py

---
+++ APIChatGPT_m2.py
@@ -0,0 +1,19 @@
+import requests
+URL = "https://api.openai.com/v1/chat/completions"
+payload = {"model": "gpt-3.5-turbo",
+           "messages": [{"role": "user", "content": f"What is the first computer in the world?"}],
+           "temperature" : 1.0,
+           "top_p":1.0,
+           "n" : 1,
+           "stream": False,
+           "presence_penalty":0,
+           "frequency_penalty":0,
+          }
+
+headers = {
+"Content-Type": "application/json",
+"Authorization": f"Bearer {openai.api_key}"
+}
+
+response = requests.post(URL, headers=headers, json=payload, stream=False)
+response.content
