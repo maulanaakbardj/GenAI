aabcaa9688c06b3d2b9a020e13107e08d6921875
Maulana Akbar Dwijaya <67249292+maulanaakbardj@users.noreply.github.com>
2023-04-29 10:47:58 +0700

    Update TextImageGAN.java

--- Java/TextImageGAN.java
+++ Java/TextImageGAN.java
@@ -1,69 +1,258 @@
-import org.tensorflow.Graph;
-import org.tensorflow.Session;
-import org.tensorflow.Tensor;
-import org.tensorflow.TensorFlow;
-
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.conf.GradientNormalization;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.conf.layers.UpSamplingLayer;
+import org.deeplearning4j.nn.conf.layers.samediff.SameDiffLambdaLayer;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.layers.normalization.BatchNormalization;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.nd4j.linalg.schedule.ExponentialSchedule;
+import org.nd4j.linalg.schedule.ScheduleType;
+import org.nd4j.linalg.util.ArrayUtil;
+import org.nd4j.linalg.util.FeatureUtil;
+import org.nd4j.linalg.util.Shape;
+
+import static org.deeplearning4j.nn.conf.layers.ConvolutionLayer.*;
 
 public class TextToImageGAN {
+    // specify the size of the image and the size of the latent variable
+    private static int IMAGE_SIZE = 64;
+    private static int LATENT_SIZE = 100;
 
-    private static final int BATCH_SIZE = 32;
-    private static final int NOISE_SIZE = 100;
-    private static final int IMAGE_SIZE = 64;
+    // specify the number of epochs to train the GAN for
+    private static int NUM_EPOCHS = 1000;
 
-    private static byte[] readAllBytesOrExit(Path path) {
-        try {
-            return Files.readAllBytes(path);
-        } catch (Exception e) {
-            System.err.println("Failed to read [" + path + "]: " + e.getMessage());
-            System.exit(1);
+    // specify the size of the batch to use for training
+    private static int BATCH_SIZE = 64;
+
+    // specify the learning rate and the beta values for the Adam optimizer
+    private static double LEARNING_RATE = 0.0002;
+    private static double BETA1 = 0.5;
+
+    // specify the location of the text file containing the descriptions
+    private static String TEXT_FILE = "descriptions.txt";
+
+    public static void main(String[] args) throws IOException {
+        // load the descriptions from the text file
+        List<String> descriptions = loadDescriptions(TEXT_FILE);
+
+        // create the generator and discriminator models
+        ComputationGraph generator = createGenerator();
+        ComputationGraph discriminator = createDiscriminator();
+
+        // set up the input and output arrays for the generator and discriminator models
+        INDArray[] generatorInputs = new INDArray[] { Nd4j.rand(new int[] { BATCH_SIZE, LATENT_SIZE }) };
+        INDArray[] discriminatorInputs = new INDArray[] { Nd4j.zeros(new int[] { BATCH_SIZE, 3, IMAGE_SIZE, IMAGE_SIZE }) };
+        INDArray[] discriminatorOutputs = new INDArray[] { Nd4j.zeros(new int[] { BATCH_SIZE, 1 }), Nd4j.zeros(new int[] { BATCH_SIZE, 1 }) };
+
+        // create the data set iterator for the images
+        DataSetIterator imageIterator = createImageIterator(BATCH_SIZE);
+
+        // create the image scaler
+        ImagePreProcessingScaler scaler = new ImagePreProcessingScaler(0, 1);
+
+        // create the Adam optimizer for the generator and discriminator models
+        Adam optimizer = new Adam.Builder().learningRate(LEARNING_RATE)
+            .beta1(BETA1)
+            .beta2(0.999)
+            .build();
+
+        // build the generator and discriminator models
+        ComputationGraphConfiguration generatorConfig = new NeuralNetConfiguration.Builder()
+            .seed(123)
+            .updater(optimizer)
+            .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+            .l2(0.0001)
+            .weightInit(WeightInit.XAVIER)
+            .graphBuilder()
+            .addInputs("latent")
+            .addLayer("dense1", new DenseLayer.Builder().nIn(LATENT_SIZE).nOut(512).build(), "latent")
+            .addLayer("batchNorm1", new BatchNormalization.Builder().build(), "dense1")
+            .addLayer("relu1", new SameDiffLambdaLayer(layer -> layer.activation(Activation.RELU)), "batchNorm1")
+            .addLayer("reshape1", new SameDiffLambdaLayer(layer -> layer.reshape(ArrayUtil.toInts(new long[] { BATCH_SIZE, 512, 4, 4 }))),"relu1")
+            .addLayer("upsample1", new UpSamplingLayer.Builder(2, 2).build(), "reshape1")
+            .addLayer("conv1", new ConvolutionLayer.Builder(3, 3)
+                .stride(1, 1)
+                .nIn(512)
+                .nOut(256)
+                .convolutionMode(ConvolutionMode.Same)
+                .build(), "upsample1")
+            .addLayer("batchNorm2", new BatchNormalization.Builder().build(), "conv1")
+        .addLayer("relu2", new SameDiffLambdaLayer(layer -> layer.activation(Activation.RELU)), "batchNorm2")
+        .addLayer("upsample2", new UpSamplingLayer.Builder(2, 2).build(), "relu2")
+        .addLayer("conv2", new ConvolutionLayer.Builder(3, 3)
+            .stride(1, 1)
+            .nIn(256)
+            .nOut(128)
+            .convolutionMode(ConvolutionMode.Same)
+            .build(), "upsample2")
+        .addLayer("batchNorm3", new BatchNormalization.Builder().build(), "conv2")
+        .addLayer("relu3", new SameDiffLambdaLayer(layer -> layer.activation(Activation.RELU)), "batchNorm3")
+        .addLayer("upsample3", new UpSamplingLayer.Builder(2, 2).build(), "relu3")
+        .addLayer("conv3", new ConvolutionLayer.Builder(3, 3)
+            .stride(1, 1)
+            .nIn(128)
+            .nOut(64)
+            .convolutionMode(ConvolutionMode.Same)
+            .build(), "upsample3")
+        .addLayer("batchNorm4", new BatchNormalization.Builder().build(), "conv3")
+        .addLayer("relu4", new SameDiffLambdaLayer(layer -> layer.activation(Activation.RELU)), "batchNorm4")
+        .addLayer("conv4", new ConvolutionLayer.Builder(3, 3)
+            .stride(1, 1)
+            .nIn(64)
+            .nOut(3)
+            .convolutionMode(ConvolutionMode.Same)
+            .build(), "relu4")
+        .addLayer("tanh", new SameDiffLambdaLayer(layer -> layer.activation(Activation.TANH)), "conv4")
+        .addLayer("out", new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
+            .activation(Activation.SOFTMAX)
+            .nIn(64)
+            .nOut(1)
+            .build(), "relu1")
+            .addLayer("out", new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                .activation(Activation.IDENTITY)
+                .nIn(256)
+                .nOut(1)
+                .build(), "relu2")
+            .setOutputs("out")
+            .build();
+
+        ComputationGraph generator = new ComputationGraph(generatorConfig);
+        generator.init();
+
+        ComputationGraphConfiguration discriminatorConfig = new NeuralNetConfiguration.Builder()
+            .seed(123)
+            .updater(optimizer)
+            .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer)
+            .l2(0.0001)
+            .weightInit(WeightInit.XAVIER)
+            .graphBuilder()
+            .addInputs("input")
+            .addLayer("conv1", new ConvolutionLayer.Builder(5, 5)
+                .stride(2, 2)
+                .nIn(3)
+                .nOut(64)
+                .activation(Activation.LEAKYRELU.alpha(0.2))
+                .build(), "input")
+            .addLayer("dropout1", new DropoutLayer.Builder(DROPOUT_RATE).build(), "conv1")
+            .addLayer("conv2", new ConvolutionLayer.Builder(5, 5)
+                .stride(2, 2)
+                .nIn(64)
+                .nOut(128)
+                .activation(Activation.LEAKYRELU.alpha(0.2))
+                .build(), "dropout1")
+            .addLayer("dropout2", new DropoutLayer.Builder(DROPOUT_RATE).build(), "conv2")
+            .addLayer("conv3", new ConvolutionLayer.Builder(5, 5)
+                .stride(2, 2)
+                .nIn(128)
+                .nOut(256)
+                .activation(Activation.LEAKYRELU.alpha(0.2))
+                .build(), "dropout2")
+            .addLayer("dropout3", new DropoutLayer.Builder(DROPOUT_RATE).build(), "conv3")
+            .addLayer("conv4", new ConvolutionLayer.Builder(5, 5)
+                .stride(2, 2)
+                .nIn(256)
+                .nOut(512)
+                .activation(Activation.LEAKYRELU.alpha(0.2))
+                .build(), "dropout3")
+            .addLayer("flatten", new FlattenLayer(), "conv4")
+            .addLayer("out", new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                .activation(Activation.SIGMOID)
+                .nIn(2048)
+                .nOut(1)
+                .build(), "flatten")
+            .setOutputs("out")
+            .build();
+
+        ComputationGraph discriminator = new ComputationGraph(discriminatorConfig);
+        discriminator.init();
+
+        // GAN network
+        AdversarialTrainer trainer = new AdversarialTrainer.Builder(generator, discriminator)
+            .generatorLayerName("out")
+            .discriminatorLayerName("out")
+            .updater(optimizer)
+            .pretrain(false)
+            .build();
+
+        // training loop
+        for (int epoch = 0; epoch < EPOCHS; epoch++) {
+            LOGGER.info("Starting epoch {}", epoch + 1);
+
+            long start = System.currentTimeMillis();
+
+            trainer.fit(dataset);
+
+            LOGGER.info("Completed epoch {} in {}ms", epoch + 1, System.currentTimeMillis() - start);
         }
-        return null;
     }
 
-    public static void main(String[] args) {
-        // Load the text dataset
-        Path textFile = Paths.get("text_dataset.txt");
-        String text = new String(readAllBytesOrExit(textFile), StandardCharsets.UTF_8);
-        String[] lines = text.split("\n");
-        List<String> textList = new ArrayList<>();
-        for (String line : lines) {
-            textList.add(line);
+    /**
+     * Method to generate and save the generated images to disk.
+     *
+     * @param generator the generator model
+     * @param num
+      * @param batchSize the batch size for generating images
+ * @param saveDir the directory to save the generated images
+ * @param epoch the epoch number (used in naming the files)
+ * @throws IOException if an I/O error occurs while saving the images
+ */
+private static void generateAndSaveImages(ComputationGraph generator, int batchSize, File saveDir, int epoch)
+    throws IOException {
+    // generate the images
+    INDArray noise = Nd4j.randn(new int[] {batchSize, NOISE_SIZE, 1, 1});
+    INDArray generatedImages = generator.outputSingle(false, noise);
+
+    // convert to JavaCV Mat objects
+    List<Mat> generatedMats = new ArrayList<>();
+    for (int i = 0; i < batchSize; i++) {
+        // get the i-th image
+        INDArray image = generatedImages.getRow(i).dup();
+
+        // scale to 0-255
+        image = image.mul(127.5).add(127.5);
+
+        // convert to 3D INDArray
+        image = image.reshape(3, IMAGE_SIZE, IMAGE_SIZE);
+
+        // convert to Mat object
+        Mat mat = new Mat(IMAGE_SIZE, IMAGE_SIZE, CV_8UC3);
+        for (int j = 0; j < 3; j++) {
+            INDArray channel = image.getRow(j);
+            Mat channelMat = new Mat(IMAGE_SIZE, IMAGE_SIZE, CV_8UC1, new BytePointer(channel.data().asBytes()));
+            channelMat.copyTo(mat.row(j));
         }
 
-        // Load the image dataset
-        Path imageFolder = Paths.get("image_dataset");
-        List<Path> imagePathList = new ArrayList<>();
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(imageFolder)) {
-            for (Path imagePath : stream) {
-                imagePathList.add(imagePath);
-            }
-        } catch (Exception e) {
-            System.err.println("Failed to read image dataset: " + e.getMessage());
-            System.exit(1);
-        }
+        generatedMats.add(mat);
+    }
 
-        // Initialize TensorFlow
-        try (Graph graph = new Graph()) {
-            byte[] model = readAllBytesOrExit(Paths.get("model.pb"));
-            graph.importGraphDef(model);
-            try (Session session = new Session(graph)) {
-                // Generate the images
-                for (String textLine : textList) {
-                    Tensor<String> input = Tensor.create(new String[]{textLine});
-                    Tensor<Float> noise = Tensor.create(new float[][]{new float[NOISE_SIZE]});
-                    Tensor<Float> output = session
-                            .runner()
-                            .feed("input_text", input)
-                            .feed("noise", noise)
-                            .fetch("output_image")
-                            .run()
-                            .get(0)
-                            .expect(Float.class);
+    // save the images to disk
+    for (int i = 0; i < batchSize; i++) {
+        String fileName = String.format("generated_%d_%d.jpg", epoch + 1, i + 1);
+        File file = new File(saveDir, fileName);
+        imwrite(file.getAbsolutePath(), generatedMats.get(i));
+    }
 
-                   
+    LOGGER.info("Generated images saved to disk");
+}
+}
+
+
